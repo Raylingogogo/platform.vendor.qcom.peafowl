2ba53c911060170218d11d5373f1fce175324d5c
Maulik Shah <mkshah@codeaurora.org>
2017-09-21 11:25:36 +0530

    sdm670: Enable suspend during power off charging
    Enable suspend during power off charging.
    
    Change-Id: Idfa8117ef8e9c937988fa3a21833ed5d4261e957

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,8 @@ BOARD_VENDOR_KERNEL_MODULES := \
     $(KERNEL_MODULES_OUT)/audio_machine_sdm670.ko \
     $(KERNEL_MODULES_OUT)/llcc_perfmon.ko
 
+# Enable suspend during charger mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
 
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
