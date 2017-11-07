abcb6fbb118b398441bd473b7c893775653bc5e4
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2017-11-07 14:32:39 +0530

    sdm670: use sdm670 config for msm_irqbalance
    Use sdm670 config file instead of sdm845 for msm_irqbalance.
    sdm670 base is copied from 845, but conf is not updated for sdm670.
    
    Change-Id: Ibf2ddf7389e43f979e5448fd955a59588d653b66
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- sdm670.mk
+++ sdm670.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
     device/qcom/sdm670/init.qti.qseecomd.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.qseecomd.sh
 
 # MSM IRQ Balancer configuration file
-PRODUCT_COPY_FILES += device/qcom/sdm845/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+PRODUCT_COPY_FILES += device/qcom/sdm670/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Camera configuration file. Shared by passthrough/binderized camera HAL
 PRODUCT_PACKAGES += camera.device@3.2-impl
