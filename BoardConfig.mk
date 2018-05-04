a0e7815f92f8cc27cb2d57f9c84d2261378c9837
Neeraj Soni <neersoni@codeaurora.org>
2018-05-04 15:46:03 +0530

    sdm670: Enable new FDE design
    New FDE design to solve regression with old
    request device mapper based implementation.
    
    Change-Id: I380c17f31c21231fc79b5dc43cd6d44fb5e2bcd6
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 
 #Enable HW based full disk encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_HW_DISK_ENCRYPT_NEW := true
 TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 
 BOARD_SECCOMP_POLICY := device/qcom/$(TARGET_BOARD_PLATFORM)/seccomp
